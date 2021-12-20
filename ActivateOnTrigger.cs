5dd014b501ed6f26bc7e841f7e7542066c8321f4
Xolani T Mabuza <mabuzaxolani862@gmail.com>
2021-12-20 15:28:51 +0200

    Update ActivateOnTrigger.cs

--- ActivateOnTrigger.cs
+++ ActivateOnTrigger.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//cool 
 public class ActivateOnTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
